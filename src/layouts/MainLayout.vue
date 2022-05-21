1fd031611f5eb0524f676c59b138e3baaced32de
Shiropetite <59747311+Shiropetite@users.noreply.github.com>
2022-05-21 23:26:43 +0200

    begin project list
    Co-authored-by: Radalls <Radalls@users.noreply.github.com>

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,45 +1,17 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header class="header">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-img src="logo.png" style="width: 40px" />
 
-        <q-toolbar-title>
-          Quasar App
+        <q-toolbar-title class="q-ml-md">
+          <h1>Creascio <span class="light">Dojo</span> </h1>
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
+    <q-page-container class="q-pa-md">
       <router-view />
     </q-page-container>
   </q-layout>
@@ -47,7 +19,6 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import EssentialLink from 'components/EssentialLink.vue';
 
 const linksList = [
   {
@@ -97,10 +68,6 @@ const linksList = [
 export default defineComponent({
   name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
-
   setup () {
     const leftDrawerOpen = ref(false)
 
@@ -114,3 +81,17 @@ export default defineComponent({
   }
 });
 </script>
+
+<style lang="scss">
+.header {
+  background-color: transparent;
+  padding: 16px;
+  border-bottom: solid 3px $primary;
+
+  .light {
+    padding-left: 8px;
+    font-weight: 500;
+    font-size: 1.8rem;
+  }
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,49 +1,170 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="home-list row justify-center">
+
+    <div class="col-xs-12 col-md-8">
+      <div class="row q-gutter-md justify-between items-center">
+
+        <q-btn-dropdown
+          class="btn"
+          color="primary"
+          icon="settings"
+          :label="`Tech : ${technology}`"
+          :ripple="false"
+          no-icon-animation
+          no-caps
+        >
+          <q-list>
+
+            <q-item clickable v-close-popup @click="technology = 'Tous'">
+              <q-item-section>
+                <q-item-label>Tous</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-close-popup @click="technology = 'Javascript'">
+              <q-item-section>
+                <q-item-label>Javascript</q-item-label>
+              </q-item-section>
+            </q-item>
+
+          </q-list>
+        </q-btn-dropdown>
+
+        <q-btn-dropdown
+          class="btn"
+          color="primary"
+          icon="north_east"
+          :label="`Niveau : ${level}`"
+          :ripple="false"
+          no-icon-animation
+          no-caps
+        >
+          <q-list>
+
+            <q-item clickable v-close-popup @click="level = 'Tous'">
+              <q-item-section>
+                <q-item-label>Tous</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-close-popup @click="level = 'Débutant'" >
+              <q-item-section>
+                <q-item-label>Débutant</q-item-label>
+              </q-item-section>
+            </q-item>
+
+          </q-list>
+        </q-btn-dropdown>
+
+        <q-space />
+
+        <q-input class="input" v-model="search" placeholder="Rechercher... " dense borderless  >
+          <template v-slot:prepend>
+            <q-icon name="search" color="primary" />
+          </template>
+        </q-input>
+
+      </div>
+    </div>
+
+    <div class="list col-xs-12 col-md-8 q-gutter-y-md">
+      <div v-for="project in projects" :key="project.id" class="card row">
+        <div class="image"></div>
+
+        <div class="col q-pt-sm">
+          <div class="row q-gutter-x-lg q-pb-md">
+            <div>
+               <div class="row items-center">
+                <q-icon name="settings" />
+                <div class="q-ml-sm">{{ project.tech }}</div>
+              </div>
+            </div>
+
+            <div>
+              <div class="row items-center">
+                <q-icon name="north_east" />
+                <div class="q-ml-sm">{{ project.level }}</div>
+              </div>
+            </div>
+
+            <div>
+              <div class="row items-center">
+                <q-icon name="schedule" />
+                <div class="q-ml-sm">{{ project.time }} min</div>
+              </div>
+            </div>
+          </div>
+
+          <h2 class="q-pt-sm">{{ project.title }}</h2>
+          <div class="text-justify">{{ project.description }}</div>
+        </div>
+      </div>
+    </div>
+
   </q-page>
 </template>
 
-<script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { defineComponent, ref } from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue';
+import projects from 'src/data/projects';
 
-export default defineComponent({
-  name: 'IndexPage',
-  components: { ExampleComponent },
-  setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
-});
+const technology = ref('Tous');
+const level = ref('Tous')
+const search = ref('');
+
 </script>
+
+<style lang="scss">
+.home-list {
+  margin-top: 60px;
+  min-height: 0 !important;
+
+  .list {
+    margin-top: 40px;
+  }
+
+  .card {
+    padding: 12px;
+    padding-right: 20px;
+    min-height: 180px;
+    background-color: $less-dark;
+    border-radius: 6px;
+    cursor: pointer;
+    transition: linear 0.2s;
+
+    &:hover {
+      background-color: #24243b;
+    }
+
+    .image {
+      margin-right: 20px;
+      width: 270px;
+      height: 160px;
+      background-color: $dark;
+      border-radius: 6px;
+    }
+  }
+
+  .btn {
+    width: 220px;
+    height: 30px;
+  }
+
+  .input {
+    padding: 0 20px;
+    background-color: $less-dark;
+    border-radius: 6px;
+    transition: linear 0.2s;
+
+    &:hover {
+      background-color: #24243b;
+    }
+
+    input {
+      width: 180px;
+      color: white;
+    }
+  }
+
+}
+</style>
